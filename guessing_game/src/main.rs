c02164875d1dcd4ff738b18b7675d840b04dae30
LucasBrentano <brentano.l@gmail.com>
2022-10-29 11:49:18 -0300

    guessing game complete

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is {secret_number}");
+    // println!("The secret number is {secret_number}");
 
     loop {
         let mut guess = String::new();
@@ -18,7 +18,13 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please input your guess");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("You must type a number!");
+                continue;
+            }
+        };
 
         println!("You guessed: {guess}");
 
